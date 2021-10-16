7d6f8b5d504ad31b968a18e7a283a7e2eec8616c
Adam Ogorek <ogoreka@tcd.ie>
2021-10-16 14:54:57 +0100

    Server now responds to queries containing city name and optionally country name with a json containing weather data

--- server.js
+++ server.js
@@ -10,38 +10,31 @@ const path = require("path")
 let publicPath = path.resolve(__dirname, "public")
 let data = fs.readFileSync('API.json')
 const key = JSON.parse(data)["key"]
-let city = 'Dublin'
-let country = 'Ireland'
-let countryCode = countries.getAlpha2Code(country, 'en')
-let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${key}`
 
-
-axios.get(url)
-  .then(function (response) {
-    console.log(response.data)
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
-  .then(function () {
-
-  })
 
 app.use(express.static(publicPath))
 
-app.get('/random/:min/:max', sendrandom)
+app.get('/weather/:city/:country?', getWeather)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-function sendrandom (req, res) {
-  let min = parseInt(req.params.min)
-  let max = parseInt(req.params.max)
-  if (isNaN(min) || isNaN(max)) {
-    res.status(400)
-    res.json({error: "Bad request."})
-    return
+function getWeather (req, res) {
+  let city = req.params.city
+  let country = req.params.country
+  let url = ''
+  if(typeof(country) === 'undefined') {
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`
+  } else {
+    let countryCode = countries.getAlpha2Code(country, 'en')
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${key}`
   }
-  let result = Math.round((Math.random() * (max-min)) + min)
-
-  res.json({result:result})
+  axios.get(url)
+    .then(function (response) {
+      res.json(response.data)
+    })
+    .catch(function (error) {
+      res.send(error)
+    })
+    .then(function () {
+    })
 }
